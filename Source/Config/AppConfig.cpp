3370aeb8953f57e4838214e6acb0930ecdb8d50c
magiruuvelvet <magiruuvelvet@naver.com>
2018-08-21 18:03:18 +0200

    app: release v0.5.1

--- Source/Config/AppConfig.cpp
+++ Source/Config/AppConfig.cpp
@@ -7,8 +7,8 @@ const std::string AppConfig::Developer = "マギルゥーベルベット";
 const std::string AppConfig::Name = "OTPGen";
 
 const std::uint16_t AppConfig::VersionMajor = 0U;
-const std::uint16_t AppConfig::VersionMinor = 4U;
-const std::uint32_t AppConfig::PatchLevel = 128U;
+const std::uint16_t AppConfig::VersionMinor = 5U;
+const std::uint32_t AppConfig::PatchLevel = 1U;
 const std::string AppConfig::Version =
     std::to_string(AppConfig::VersionMajor) + '.' +
     std::to_string(AppConfig::VersionMinor) + '.' +
