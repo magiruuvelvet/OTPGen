cc4839aed8456e69dd15bc10d2ae9c4a272972af
magiruuvelvet <magiruuvelvet@naver.com>
2018-08-24 18:47:28 +0200

    core: implemented otpauth uri parser

---
+++ Source/Core/Tools/otpauthURI.cpp
@@ -0,0 +1,238 @@
+#include "otpauthURI.hpp"
+
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <regex>
+
+const std::string otpauthURI::OTPAUTH_PREFIX = "otpauth://";
+
+otpauthURI::otpauthURI()
+{
+}
+
+otpauthURI::otpauthURI(const std::string &uri)
+{
+    if (uri.size() >= OTPAUTH_PREFIX.size() && uri.substr(0, OTPAUTH_PREFIX.size()) == OTPAUTH_PREFIX)
+    {
+        this->uri = uri.substr(OTPAUTH_PREFIX.size());
+    }
+
+    if (!empty())
+    {
+        // extract type
+        auto delim = this->uri.find_first_of('/');
+        if (delim == std::string::npos)
+        {
+            setTypeEnum(this->uri);
+            return;
+        }
+        else
+        {
+            setTypeEnum(this->uri.substr(0, delim));
+            if (_type == Invalid)
+            {
+                return;
+            }
+        }
+
+        ++delim;
+
+        // extract label, use URI decoding
+        auto delim2 = this->uri.find_first_of('?', delim);
+        if (delim2 == std::string::npos)
+        {
+            UriComponent label(this->uri.substr(delim));
+            _label = label.decoded();
+            return;
+        }
+        else
+        {
+            UriComponent label(this->uri.substr(delim, delim2 - delim));
+            _label = label.decoded();
+        }
+
+        ++delim2;
+
+        // extract parameters
+        auto params_str = this->uri.substr(delim2);
+        if (params_str.empty() || params_str.find('=') == std::string::npos)
+        {
+            return;
+        }
+
+        std::regex re(R"(\&)");
+        std::sregex_token_iterator first{
+            params_str.begin(), params_str.end(), re, -1
+        }, last;
+        std::vector<std::string> params_vec = {first, last};
+
+        for (auto&& p : params_vec)
+        {
+            auto delim = p.find('=');
+            if (delim == std::string::npos)
+            {
+                continue;
+            }
+
+            auto key = p.substr(0, delim);
+            auto value = p.substr(delim + 1);
+
+            // URI decode issuer
+            if (key == "issuer")
+            {
+                UriComponent issuer(value);
+                value = issuer.decoded();
+            }
+
+            _params.insert({key, value});
+        }
+
+        // parameters may not be empty
+        if (_params.empty())
+        {
+            return;
+        }
+
+        // check for mandatory fields
+        if (_params.count("secret") != 1)
+        {
+            _params.clear();
+            return;
+        }
+        if (_type == HOTP && _params.count("counter") != 1)
+        {
+            _params.clear();
+            return;
+        }
+
+        // add defaults when missing
+        if (_params.count("algorithm") == 0)
+        {
+            _params.insert({"algorithm", "SHA1"});
+        }
+        if (_params.count("digits") == 0)
+        {
+            _params.insert({"digits", "6"});
+        }
+
+        if (_type == TOTP && _params.count("period") == 0)
+        {
+            _params.insert({"period", "30"});
+        }
+
+        // set valid if reached here
+        _valid = true;
+    }
+}
+
+otpauthURI::~otpauthURI()
+{
+    uri.clear();
+    _label.clear();
+    _params.clear();
+}
+
+void otpauthURI::setTypeEnum(const std::string &type)
+{
+    if (type == "totp")
+    {
+        _type = TOTP;
+    }
+    else if (type == "hotp")
+    {
+        _type = HOTP;
+    }
+    else
+    {
+        _type = Invalid;
+    }
+}
+
+otpauthURI::UriComponent::UriComponent(const std::string &uri, const std::string &prefix)
+    : prefix(prefix), uri(uri)
+{
+    if (uri.size() >= prefix.size() && uri.substr(0, prefix.size()) == prefix)
+    {
+        this->uri = uri.substr(prefix.size());
+    }
+    else
+    {
+        this->prefix.clear();
+    }
+}
+
+otpauthURI::UriComponent::~UriComponent()
+{
+    prefix.clear();
+    uri.clear();
+}
+
+const std::string otpauthURI::UriComponent::encoded() const
+{
+    std::string new_str = "";
+    char c;
+    int ic;
+    const char *chars = uri.c_str();
+    char bufHex[10];
+    auto len = strlen(chars);
+
+    for (auto i = 0U; i < len; i++)
+    {
+        c = chars[i];
+        ic = c;
+        if (c == ' ')
+        {
+            new_str += '+';
+        }
+        else if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+        {
+            new_str += c;
+        }
+        else
+        {
+            std::sprintf(bufHex, "%X", c);
+            if (ic < 16)
+            {
+                new_str += "%0";
+            }
+            else
+            {
+                new_str += "%";
+            }
+            new_str += bufHex;
+        }
+    }
+    return prefix + new_str;
+}
+
+const std::string otpauthURI::UriComponent::decoded() const
+{
+    std::string ret;
+    char ch;
+    unsigned int i, ii;
+    auto len = uri.length();
+
+    for (i = 0U; i < len; i++)
+    {
+        if (uri[i] != '%')
+        {
+            if (uri[i] == '+')
+            {
+                ret += ' ';
+            }
+            else
+            {
+                ret += uri[i];
+            }
+        }
+        else
+        {
+            std::sscanf(uri.substr(i + 1, 2).c_str(), "%x", &ii);
+            ch = static_cast<char>(ii);
+            ret += ch;
+            i = i + 2;
+        }
+    }
+    return prefix + ret;
+}
---
+++ Source/Core/Tools/otpauthURI.hpp
@@ -0,0 +1,126 @@
+#ifndef OTPAUTHURI_HPP
+#define OTPAUTHURI_HPP
+
+#include <map>
+#include <string>
+
+/**
+ * otpauth URI
+ *
+ * see: https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+ *
+ * otpauth://TYPE/LABEL?PARAMETERS
+ *
+ * Examples:
+ * otpauth://totp/Example:alice@google.com?secret=JBSWY3DPEHPK3PXP&issuer=Example
+ * otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30
+ *
+ * Parameters:
+ *
+ *  => secret (REQUIRED)
+ *  => issuer (OPTIMAL, unused in this application)
+ *  => algorithm (OPTIMAL, default is "SHA1")
+ *  => digits (OPTIMAL, default is "6")
+ *  => counter (REQUIRED if type is "HOTP", otherwise unused)
+ *  => period (OPTIMAL only if type is "TOTP", otherwise unused, default is "30")
+ *
+ * Note about issuer prefixes:
+ *
+ *  This application doesn't use "issuers", the prefix is part of the label as-is.
+ *
+ */
+
+// TODO: currently only parsing is supported
+// TODO: write generator
+
+class otpauthURI
+{
+    class UriComponent;
+
+public:
+    otpauthURI();
+    otpauthURI(const std::string &uri);
+    ~otpauthURI();
+
+    enum Type {
+        Invalid = -1,
+        TOTP,
+        HOTP,
+    };
+
+    inline const auto &type() const
+    { return _type; }
+
+    inline const auto &label() const
+    { return _label; }
+
+    inline const auto &secret() const
+    { return _params.at("secret"); }
+    inline const auto &algorithm() const
+    { return _params.at("algorithm"); }
+    inline const auto &digits() const
+    { return _params.at("digits"); }
+
+    inline const std::string &counter() const
+    {
+        static const std::string empty;
+        if (_params.count("counter") != 0)
+        {
+            return _params.at("counter");
+        }
+        else
+        {
+            return empty;
+        }
+    }
+
+    inline const std::string &period() const
+    {
+        static const std::string empty;
+        if (_params.count("period") != 0)
+        {
+            return _params.at("period");
+        }
+        else
+        {
+            return empty;
+        }
+    }
+
+    inline const auto &params() const
+    { return _params; }
+
+    inline bool empty() const
+    { return uri.empty(); }
+
+    inline bool valid() const
+    { return _valid; }
+
+private:
+    static const std::string OTPAUTH_PREFIX;
+    std::string uri;
+    bool _valid = false;
+
+    Type _type = Invalid;
+    std::string _label;
+    std::map<std::string, std::string> _params;
+
+    void setTypeEnum(const std::string &type);
+
+    // helper class to decode and encode URI components (percent signs)
+    class UriComponent
+    {
+    public:
+        UriComponent(const std::string &uri, const std::string &prefix = OTPAUTH_PREFIX);
+        ~UriComponent();
+
+        const std::string encoded() const;
+        const std::string decoded() const;
+
+    private:
+        std::string prefix;
+        std::string uri;
+    };
+};
+
+#endif // OTPAUTHURI_HPP
