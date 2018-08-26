5e0513b5f4d695897f966059dc4e16f3791973ff
magiruuvelvet <magiruuvelvet@naver.com>
2018-08-26 12:08:20 +0200

    main: initial CLI functionality

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <cstdlib>
 #include <cstdio>
 
@@ -232,7 +234,15 @@ int start(QtSingleApplication *a, const std::string &keychainPassword, bool crea
         else if (message.compare("reloadTokens", Qt::CaseInsensitive) == 0)
         {
             std::printf("Trying to reload the token database...\n");
-            mainWindow->updateTokenList();
+            if (TokenDatabase::loadTokens() == TokenDatabase::Success)
+            {
+                std::printf("Updated!\n");
+                mainWindow->updateTokenList();
+            }
+            else
+            {
+                std::printf("Failed to update the token database!\n");
+            }
         }
     });
 
@@ -347,13 +357,69 @@ int main(int argc, char **argv)
     return a.exec();
 }
 
-#endif
+#endif // OTPGEN_GUI
 
 #ifdef OTPGEN_CLI
 
+#include <Cli/StdinEchoMode.hpp>
+
+#include <sago/platform_folders.h>
+
 int main(int argc, char **argv)
 {
-    std::printf("Hello!\n");
+    std::printf("%s CLI\n\n", cfg::Name.c_str());
+
+#ifdef OTPGEN_DEBUG
+    TokenDatabase::setPassword("pwd123");
+#else
+    std::string password;
+    std::cout << "Enter your token database password: ";
+
+    SetStdinEcho(false);
+    std::cin >> password;
+    SetStdinEcho(true);
+
+    if (!TokenDatabase::setPassword(password))
+    {
+        std::cerr << "Password may not be empty!" << std::endl;
+        return 1;
+    }
+
+    password.clear();
+
+    std::cout << std::endl;
+#endif
+
+    const auto config_home = sago::getConfigHome();
+    const auto app_cfg = config_home + "/" + cfg::Developer + "/" + cfg::Name;
+
+#ifdef OTPGEN_DEBUG
+    TokenDatabase::setTokenFile(app_cfg + "/database.debug");
+#else
+    TokenDatabase::setTokenFile(app_cfg + "/database");
+#endif
+
+    auto status = TokenDatabase::loadTokens();
+    if (status == TokenDatabase::FileReadFailure)
+    {
+        status = TokenDatabase::saveTokens();
+        if (status != TokenDatabase::Success)
+        {
+            std::cerr << "Unable to initialize the token database!" << std::endl;
+            return 1;
+        }
+    }
+    if (status != TokenDatabase::Success)
+    {
+        std::cerr << "Unable to load the token database! Is the password correct?" << std::endl;
+        return 1;
+    }
+
+    // run command line operation if any
+    const std::vector<std::string> args(argv, argv + argc);
+    exec_commandline_operation(args);
+
+    return 0;
 }
 
-#endif
+#endif // OTPGEN_CLI
