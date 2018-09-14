9d1326d62f00fe7073cd502404bf4fdac1fb2cb8
magiruuvelvet <magiruuvelvet@naver.com>
2018-09-14 13:09:11 +0200

    android: fix native lib symbol names

--- Android/app/src/main/cpp/libotpgen-bindings.cpp
+++ Android/app/src/main/cpp/libotpgen-bindings.cpp
@@ -36,7 +36,7 @@ namespace {
 
 JNI_C_LINKAGE {
 
-JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeTOTP)(JNIEnv *env, jobject,
+JSTRING JNI_FUNCTION(Tokens_OTPGen, 00024Companion__1computeTOTP)(JNIEnv *env, jobject,
     jstring secret, jint digits, jint period, jint algo)
 {
     auto err = OTPGenErrorCode::Valid;
@@ -48,7 +48,7 @@ JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeTOTP)(JNIEnv *env, jobject,
     return validateTokenResultAndReturn(env, err, token);
 }
 
-JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeTOTP_1AtTime)(JNIEnv *env, jobject,
+JSTRING JNI_FUNCTION(Tokens_OTPGen, 00024Companion__1computeTOTP_1AtTime)(JNIEnv *env, jobject,
     jstring secret, jint digits, jint period, jint algo, jlong time)
 {
     auto err = OTPGenErrorCode::Valid;
@@ -61,7 +61,7 @@ JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeTOTP_1AtTime)(JNIEnv *env, 
     return validateTokenResultAndReturn(env, err, token);
 }
 
-JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeHOTP)(JNIEnv *env, jobject,
+JSTRING JNI_FUNCTION(Tokens_OTPGen, 00024Companion__1computeHOTP)(JNIEnv *env, jobject,
     jstring secret, jint digits, jint counter, jint algo)
 {
     auto err = OTPGenErrorCode::Valid;
@@ -73,7 +73,7 @@ JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeHOTP)(JNIEnv *env, jobject,
     return validateTokenResultAndReturn(env, err, token);
 }
 
-JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeSteam)(JNIEnv *env, jobject,
+JSTRING JNI_FUNCTION(Tokens_OTPGen, 00024Companion__1computeSteam)(JNIEnv *env, jobject,
     jstring secret)
 {
     auto err = OTPGenErrorCode::Valid;
@@ -84,7 +84,7 @@ JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeSteam)(JNIEnv *env, jobject
     return validateTokenResultAndReturn(env, err, token);
 }
 
-JSTRING JNI_FUNCTION(OTPGen, 00024Companion__1computeSteam_1AtTime)(JNIEnv *env, jobject,
+JSTRING JNI_FUNCTION(Tokens_OTPGen, 00024Companion__1computeSteam_1AtTime)(JNIEnv *env, jobject,
     jstring secret, jlong time)
 {
     auto err = OTPGenErrorCode::Valid;
