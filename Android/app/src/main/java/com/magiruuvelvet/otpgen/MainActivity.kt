4cba869536fe70932c09528bf04b9d467ad90d97
magiruuvelvet <magiruuvelvet@naver.com>
2018-09-15 16:35:22 +0200

    android: add SettingsActivity stub

--- Android/app/src/main/java/com/magiruuvelvet/otpgen/MainActivity.kt
+++ Android/app/src/main/java/com/magiruuvelvet/otpgen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.magiruuvelvet.otpgen
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -34,13 +35,16 @@ class MainActivity : AppCompatActivity()
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId)
+        if (item.itemId == R.id.action_settings)
         {
-            R.id.action_settings -> true;
-            else -> super.onOptionsItemSelected(item);
+            startActivity(Intent(this, SettingsActivity::class.java));
+            return true;
         }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
+
+        return false;
     }
 }
