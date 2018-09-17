9f0554239908a9e0191ee9a6e57e06c39ab6f7f1
magiruuvelvet <magiruuvelvet@naver.com>
2018-09-17 08:44:05 +0200

    android: add QR code unit test
     > commented out due to problems with raw resources
       having the wrong file size after writing them
       to disk

--- Android/app/src/androidTest/java/com/magiruuvelvet/otpgen/DeviceUnitTests.kt
+++ Android/app/src/androidTest/java/com/magiruuvelvet/otpgen/DeviceUnitTests.kt
@@ -2,6 +2,10 @@ package com.magiruuvelvet.otpgen
 
 import com.magiruuvelvet.libotpgen.OTPGen
 import com.magiruuvelvet.libotpgen.OTPToken
+import com.magiruuvelvet.libotpgen.libotpgen
+
+// import com.magiruuvelvet.otpgen.Utils.ResourceManager
+// import com.magiruuvelvet.otpgen.test.R;
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -29,6 +33,24 @@ class DeviceUnitTests
         val appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("com.magiruuvelvet.otpgen", appContext.packageName);
     }
+
+    // FIXME: test currently failing, wrong file size
+    //   PNG has 626 bytes, but after writing the file to the cache the size
+    //   is 852 bytes with random garbage added :/
+    // @Test
+    // fun test_qr_decode()
+    // {
+    //     val appContext = InstrumentationRegistry.getTargetContext();
+    //
+    //     // write test resource to apps cache directory
+    //     val valid_png_path = appContext.cacheDir.absolutePath + "/valid.png";
+    //     ResourceManager.createFile(valid_png_path, appContext, R.raw.valid_png);
+    //
+    //     // start QR decode test
+    //     val res = libotpgen.QRCode_decode(valid_png_path);
+    //     assertEquals(res.getStatus(), true);
+    //     assertEquals(res.getData(), "otpauth://totp/Example:alice@google.com?secret=JBSWY3DPEHPK3PXP&issuer=Example");
+    // }
 
     @Test
     fun test_totp_generation()
