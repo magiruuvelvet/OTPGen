8ba248810a838371f0021783fabe7b1094544144
magiruuvelvet <magiruuvelvet@naver.com>
2018-09-17 13:42:37 +0200

    android: implement TokenStore test

--- Android/app/src/androidTest/java/com/magiruuvelvet/otpgen/TokenTests.kt
+++ Android/app/src/androidTest/java/com/magiruuvelvet/otpgen/TokenTests.kt
@@ -1,9 +1,11 @@
 package com.magiruuvelvet.otpgen
 
+import com.magiruuvelvet.libotpgen.OTPToken;
 import com.magiruuvelvet.libotpgen.TOTPToken;
 import com.magiruuvelvet.libotpgen.HOTPToken;
 import com.magiruuvelvet.libotpgen.SteamToken;
 import com.magiruuvelvet.libotpgen.AuthyToken;
+import com.magiruuvelvet.libotpgen.TokenStore;
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -53,5 +55,38 @@ class TokenTests
         assertTrue(token.generateToken().length == token.digits().toInt());
         assertTrue(token.digits() == 7.toShort());
         assertTrue(token.period() == 10.toLong());
+    }
+
+    @Test
+    fun test_tokenstore()
+    {
+        val tokenStore = TokenStore.i();
+        assertTrue(tokenStore.empty());
+        tokenStore.addToken(TOTPToken("test", "", "secret", 7, 10, 0, OTPToken.ShaAlgorithm.SHA1));
+        assertFalse(tokenStore.empty());
+        val tokens = tokenStore.tokens();
+        assertTrue(tokens.size() == 1L);
+        assertTrue(tokenStore.tokenCount() == 1L);
+
+        // OTPTokenPtrList
+        // val test = tokens.get(0); <-- native crash
+        // assertEquals(test.label(), "test");
+        // assertEquals(test.secret(), "secret");
+        // assertTrue(test.digits() == 7.toShort());
+        // assertTrue(test.period() == 10.toLong());
+        // for (i in 0..tokens.size())
+        // {
+        //     // native crash
+        //     assertEquals(tokens[i.toInt()].label(), "test");
+        // }
+
+        val token = tokenStore.tokenAt("test");
+        assertEquals(token.label(), "test");
+        assertEquals(token.secret(), "secret");
+        assertTrue(token.digits() == 7.toShort());
+        assertTrue(token.period() == 10.toLong());
+
+        val nullToken = tokenStore.tokenAt("test2");
+        assertEquals(nullToken, null);
     }
 }
